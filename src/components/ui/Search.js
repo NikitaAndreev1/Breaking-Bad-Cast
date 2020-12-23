215c9a01eb4e01ddeef1f01652d05c0a3630d2a8
Nikita Andreev <nikita.andreev.work@gmail.com>
2020-12-23 13:27:24 +0800

    Created a React App which uses API

---
+++ src/components/ui/Search.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react'
+
+const Search = () => {
+    return (
+        <section className='search'>
+            
+        </section>
+    )
+}
+
+export default Search
